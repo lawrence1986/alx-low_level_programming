650981234fbef3f75fdb332e85551d4879b9d65f
lawrence1986 <madusquare@gmail.com>
2023-03-16 14:32:01 -0700

    Fixed spaces at the start of line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-i#include <stdio.h>
-/*
-* main - entry point
-* return: always 0 (success)
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle,\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
