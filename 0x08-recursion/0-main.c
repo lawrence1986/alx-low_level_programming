c1de424401dc5d8bd9ab2cc49bc27166029cc82f
lawrence1986 <madusquare@gmail.com>
2023-04-05 14:53:54 -0700

    Hot fix for 0-puts_recursion

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Lawrence Maduabuchi - Cohort 13
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef main_h
+#define main_h
+
+int _strlen_recursion(char *s);
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int is_palindrome(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int is_prime_number(int n);
+#endif
