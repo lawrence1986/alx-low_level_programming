65d16d50f30c8637d702fab113c1000ddfa5e044
lawrence1986 <madusquare@gmail.com>
2023-04-11 06:24:20 -0700

    Hot fix on _puts fuction

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: string to print
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: pointer to string to be printed
  */
+
 void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		_putchar(*str++);
+		_putchar(*str);
+		str++;
 	}
-		_putchar('\n');
+	_putchar('\n');
 }
