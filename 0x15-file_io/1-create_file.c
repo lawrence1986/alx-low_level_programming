a73927f7ff6f78daeaf409fb8119a83468a243ae
lawrence1986 <madusquare@gmail.com>
2023-05-08 17:02:04 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,25 +9,25 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, x, y = 0;
+	int fdd, m, n = 0;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd < 0)
+	fdd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fdd < 0)
 		return (-1);
 
 	if (text_content)
 	{
-		while (text_content[y])
-			y++;
-		x = write(fd, text_content, y);
-		if (x != y)
+		while (text_content[n])
+			n++;
+		m = write(fdd, text_content, n);
+		if (m != n)
 			return (-1);
 	}
 
-	close(fd);
+	close(fdd);
 
 	return (1);
 }
